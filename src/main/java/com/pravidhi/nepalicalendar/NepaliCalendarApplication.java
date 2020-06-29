d999ddd7968e6201a20e24f7758daee14e77367b
manojmht18 <manoj.mht18@gmail.com>
2020-06-28 19:31:36 -0500

    Initial Setup

---
+++ src/main/java/com/pravidhi/nepalicalendar/NepaliCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.pravidhi.nepalicalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepaliCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NepaliCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pravidhi/nepalicalendar/NepaliCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pravidhi.nepalicalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NepaliCalendarApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
